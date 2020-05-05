93fa00ec19bbee7d2fdae36a974f1d943a6c36bd
idolleramayana <idolleramayana@gmail.com>
2020-05-05 17:15:42 +0530

    Initial version of decent web app

---
+++ form/loginCheck.php
@@ -0,0 +1,30 @@
+<?php
+include "config.php";
+
+if(isset($_POST['submit'])) {
+
+    $uname = $_POST['email'];
+    $password = $_POST['password'];
+
+    if ($uname != "" && $password != "")
+    {
+        $sql_query = "SELECT COUNT(*) as cntUser FROM USER where email='".$uname."' and pass='".$password."'";
+        $result = mysqli_query($con,$sql_query);
+        $row = mysqli_fetch_array($result);
+
+        $count = $row['cntUser'];
+
+        if($count > 0)
+        {
+            header('Location: ../home.php');
+        }
+        else
+        {
+            header('Location: ../login.php');
+        }
+
+    }
+
+}
+
+?>
