ff5d8ae14279397d60e6a147b68cc42fe7190ff0
Shridhar Hegde <shridharkumta@gmail.com>
2020-05-06 21:28:32 +0530

    Intial web app using Scaffold template

--- login.php
+++ login.php
@@ -1,35 +1,155 @@
+<?php
+include "config.php";
+
+if(isset($_POST['submit'])) {
+
+    $uname = $_POST['email'];
+    $password = $_POST['password'];
+
+    if ($uname != "" && $password != "")
+    {
+        $sql_query = "SELECT COUNT(*) as cntUser FROM USER where email='".$uname."' and pass='".$password."'";
+        $result = mysqli_query($con,$sql_query);
+        $row = mysqli_fetch_array($result);
+
+        $count = $row['cntUser'];
+
+        if($count > 0)
+        {
+            header('Location: ../home.php');
+        }
+        else
+        {?>
+            <script> alert("Either email or password is wrong!"); </script>
+    <?php    }
+
+    }
+
+}
+
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title> </title>
-    <link rel="stylesheet" type="text/css" href="css\custom.css">
-    <?php include 'links.php' ?>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SoNA</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/main.css" rel="stylesheet" media="all">
+
 </head>
 
 <body>
 
-<div class="wrapper fadeInDown">
-  <div id="formContent">
-    <!-- Tabs Titles -->
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex">
 
-    <!-- Icon -->
-    <div class="fadeIn first">
-      <img src="https://cdn.unlockboot.com/wp-content/uploads/2018/12/type-apple-logo.jpg" id="icon" alt="User Icon" />
+      <div class="logo mr-auto">
+        <h1 class="text-light"><a href="index.html"><span>SoNA</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active">
+            <a href="index.html">Home</a>
+            <li class="drop-down"><a href="">ACCOUNT</a>
+              <ul>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="login.php">Login</a></li>
+              </ul>
+            </li>
+          </li>
+        </ul>
+      </nav><!-- .nav-menu -->
+  </header><!-- End Header -->
+
+
+  <main id="main">
+
+
+    <div class="page-wrapper bg-dark p-t-100 p-b-50">
+        <div class="wrapper wrapper--w900">
+            <div class="card card-6">
+                <div class="card-heading">
+                    <h2 class="title">Login</h2>
+                </div>
+                <div class="card-body">
+                    <form method="POST" action="login.php">
+                      <div class="form-row">
+                            <div class="name">Email address</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-6" type="email" name="email" placeholder="example@email.com" required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="name">Password</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-6" type="password" name="password" placeholder="Set a password for your account" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <button class="btn btn--radius-2 btn--blue-2" name="submit" type="submit">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 
-    LOGIN
-    <!-- Login Form -->
-    <form action="loginCheck.php" method="POST">
-      <input type="text" id="form" class="fadeIn second" name="email" placeholder="Email">
-      <input type="text" id="form" class="fadeIn third" autocomplete="off" name="password" placeholder="Password">
-      <input type="submit" name="but_submit" id="but_submit" class="fadeIn fourth" value="Log In">
-    </form>
+    <!-- Jquery JS-->
+    <script src="assets/vendor/jquery/jquery.min-2.js"></script>
 
-  </div>
-</div>
+    <!-- Main JS-->
+    <script src="assets/js/global.js"></script>
 
+  </main><!-- End #main -->
 
+  <!-- Button to go to top -->
+  <a href="#" class="back-to-top"><i class="bx bxs-up-arrow-alt"></i></a>
 
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
 
 </body>
+
 </html>
